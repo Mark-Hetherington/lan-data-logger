2ad86379381a1f624163462510be1935f80500a8
Mark Hetherington <mark@hethos.org>
2016-04-14 14:30:26 +1000

    correct unit

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from obd.OBDResponse import Unit
 def brake(messages):
     d = messages[0].data
     v = bytes_to_int(d) / 4.0  # helper function for converting byte arrays to ints
-    return (v, Unit.None)
+    return (v, Unit.NONE)
 
 imievcmd = OBDCommand("BRAKE", "Brake Pedal", "208", 8, brake,)
 
