adb000b490dda50781b5a2b1bd0ff8cc5ffc50b9
Mark Hetherington <mark@hethos.org>
2016-04-14 14:28:19 +1000

    add test script

---
+++ test.py
@@ -0,0 +1,23 @@
+import obd
+
+from obd import OBDCommand
+from obd.protocols import ECU
+from obd.utils import bytes_to_int
+from obd.OBDResponse import Unit
+
+def brake(messages):
+    d = messages[0].data
+    v = bytes_to_int(d) / 4.0  # helper function for converting byte arrays to ints
+    return (v, Unit.None)
+
+imievcmd = OBDCommand("BRAKE", "Brake Pedal", "208", 8, brake,)
+
+obd.debug.console = True
+
+connection = obd.OBD()
+
+cmd = imievcmd
+
+response = connection.query(imievcmd,force=True)
+print response.value
+print response.unit
