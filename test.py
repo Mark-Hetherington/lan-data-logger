f9bbaff2470b116a0a0060084c1d2014982f559c
Mark Hetherington <mark@hethos.org>
2016-04-14 14:44:56 +1000

    add mode to command

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import obd
 
 from obd import OBDCommand
-from obd.protocols import ECU
-from obd.utils import bytes_to_int
+# from obd.protocols import ECU
+from obd.utils import bytes_to_hex
 from obd.OBDResponse import Unit
+
+PIDs = ["119", "149"]
 
 def brake(messages):
     d = messages[0].data
-    v = bytes_to_int(d) / 4.0  # helper function for converting byte arrays to ints
+    v = bytes_to_hex(d)  # helper function for converting byte arrays to hex
     return (v, Unit.NONE)
 
-imievcmd = OBDCommand("BRAKE", "Brake Pedal", "208", 8, brake,)
+imievcmd = OBDCommand("BRAKE", "Brake Pedal", "01208", 8, brake,)
 
 obd.debug.console = True
 
